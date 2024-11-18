a7a90c7ed4e668522d59d044a7321919648eb920
MS Rahman <msrahman048gmail.com>
2024-11-18 11:24:20 +0600

    jwt implement

--- index.js
+++ index.js
@@ -25,12 +25,182 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
     // Collection
     const usersCollection = client.db("touristGuideDb").collection("users");
+    const packagesCollection = client.db("touristGuideDb").collection("packages");
+    const wishPackagesCollection = client.db("touristGuideDb").collection("wishPackages");
+    const bookingCollection = client.db("touristGuideDb").collection("booking");
 
+ // jwt related api
+ app.post('/jwt', async(req, res) =>{
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h"});
+  res.send({ token})
+})
 
+// mddleware  
+const verifyToken = (req, res, next) =>{
+  // console.log("inside verify Token", req.headers.authorization);
+  if(!req.headers.authorization){
+    return res.status(401).send({message: "unauthorized access"})
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+//  use verify admin after verifyToken
+const verifyAdmin = async(req, res, next) =>{
+const email = req.decoded.email;
+const query = {email: email};
+const user = await usersCollection.findOne(query);
+const isAdman = user?.role === 'admin';
+if(!isAdman){
+  return res.status(403).send({message: 'forbidden access'});
+}
+next();
+}
 
+  app.get("/users", verifyToken, verifyAdmin, async(req, res) =>{
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+     })
+    app.patch('/user/admin/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc ={
+        $set:{
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+    app.patch('/user/tourguide/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc ={
+        $set:{
+          role: 'tourguide'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+app.patch('/user/request/:id', async(req, res)=>{
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)};
+  const updatedDoc ={
+    $set:{
+      request: 'requested'
+    }
+  }
+  const result = await usersCollection.updateOne(filter, updatedDoc);
+  res.send(result);
+})
+
+    app.get('/user/admin/:email', verifyToken, async(req, res) =>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin'
+      }
+      res.send({admin})
+   })
+    app.get('/user/tourguide/:email', verifyToken, async(req, res) =>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      let tourguide = false;
+      if(user){
+        tourguide = user?.role === 'tourguide'
+      }
+      res.send({tourguide})
+   })
+    app.get('/user/tourist/:email', verifyToken, async(req, res) =>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      let tourist = '';
+      if(user?.role==='tourist'){
+        tourist = user
+      }
+      res.send(tourist)
+   })
+    app.get('/userinfo/:email', verifyToken, async(req, res) =>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      res.send(user)
+   })
+
+// Packages
+app.post('/package', verifyToken, verifyAdmin, async (req, res) =>{
+  const packageData = req.body
+    const result = await packagesCollection.insertOne(packageData);
+    res.send(result)
+  })
+app.get('/packages', async (req, res) =>{
+  const result = await packagesCollection.find().toArray();
+  res.send(result)
+})
+
+app.get("/package/:id", async(req, res) =>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)};
+  const result = await packagesCollection.findOne(query);
+  res.send(result)
+ })
+// wishlist api
+app.post("/wishPackage", async(req, res) =>{
+  const wishPack = req.body;
+  const result = await wishPackagesCollection.insertOne(wishPack);
+  res.send(result)
+})
+app.get("/wishPackages", async(req, res) =>{
+  const wishPack = req.body;
+  const result = await wishPackagesCollection.find().toArray();
+  res.send(result)
+ })
+ app.delete("/wishPackage/:id", async(req, res) =>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)};
+  const result = await wishPackagesCollection.deleteOne(query);
+  res.send(result)
+ })
+
+//  booking api
+
+app.post('/booking', verifyToken, async (req, res) =>{
+    const packageData = req.body
+    const result = await bookingCollection.insertOne(packageData);
+    res.send(result)
+  })
+app.get('/booking/:email', verifyToken, async (req, res) =>{
+    const email = req.params.email
+    const query = {touristEmail: email};
+    const result = await bookingCollection.find(query).toArray();
+    res.send(result)
+  })
  app.post('/signup', async (req, res) => {
   const userData = req.body
   const query = { email: userData.email };
@@ -44,7 +214,7 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
